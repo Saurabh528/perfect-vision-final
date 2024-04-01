efd758d2e6a73f6612235fae0ee7d52bfc691a1e
streetcodec <streetcodec@gmail.com>
2024-04-01 19:59:59 +0530

    Color Manager updated

--- Assets/ColorManager.cs
+++ Assets/ColorManager.cs
@@ -11,9 +11,9 @@ using UnityEngine.UI;
 public class ColorManager : MonoBehaviour
 {
     public Button VAT;
-    public GameObject Worth4DotTest;
-    public GameObject Alignment;
-    public GameObject Displacement;
+    public Button Worth4DotTest;
+    public Button Alignment;
+    public Button Displacement;
     public Button Crane2D;
     // Start is called before the first frame update
     void Start()
@@ -55,21 +55,34 @@ public class ColorManager : MonoBehaviour
             {
                 
                 int count;
-                if (!int.TryParse(result.Data["DiagnosticCount"].Value, out count))
-                {
-                    Debug.Log("Failed to parse DiagnosticCount as int.");
-                    return;
-                }
+                // if (!int.TryParse(result.Data["DiagnosticCount"].Value, out count))
+                // {
+                //     Debug.Log("Failed to parse DiagnosticCount as int.");
+                //     return;
+                // }
 
                 var currentCrane2D = result.Data["Crane2D"].Value;
-                JObject currentCrane2DJObject;
+                var currentVAT = result.Data["VAT"].Value;
+                JObject currentCrane2DJObject, currentVATJObject;
                 try
                 {
                     currentCrane2DJObject = JObject.Parse(currentCrane2D);
+                    
                 }
                 catch (Exception ex)
                 {
                     Debug.Log($"Failed to parse Crane2D as JSON: {ex.Message}");
+                    return;
+                }
+
+                try
+                {
+                    currentVATJObject = JObject.Parse(currentVAT);
+                    
+                }
+                catch (Exception ex)
+                {
+                    Debug.Log($"Failed to parse VAT as JSON: {ex.Message}");
                     return;
                 }
                 count = Int32.Parse(result.Data["DiagnosticCount"].Value);
@@ -82,17 +95,38 @@ public class ColorManager : MonoBehaviour
                     return;
                 }
 
-                JObject sessionObject = (JObject)currentCrane2DJObject[sessionNeeded];
-                string resultFoundX = (string)sessionObject?["x"];
-                if (resultFoundX != null)
+                JObject sessionObjectCrane2D = (JObject)currentCrane2DJObject[sessionNeeded];
+                JObject sessionObjectVAT = (JObject)currentVATJObject[sessionNeeded];
+                string resultFoundCrane2D = (string)sessionObjectCrane2D?["x"];
+                string resultFoundVAT = (string)sessionObjectVAT?["LeftScore"];
+                if (resultFoundCrane2D != null)
                 {
-                    Debug.Log("Result Found is " + resultFoundX);
+                    Debug.Log("Result Found is " + resultFoundCrane2D);
                     Crane2D.GetComponent<Image>().color = new Color32(0, 255, 0, 255);
                 }
                 else
                 {
-                    Debug.Log($"Result not found in session {sessionNeeded}.");
+                    Debug.Log($"Result not found for {sessionNeeded}.");
                 }
+                if (resultFoundVAT != null)
+                {
+                    Debug.Log("Result Found is " + resultFoundVAT);
+                    VAT.GetComponent<Image>().color = new Color32(0, 255, 0, 255);
+                }
+                else
+                {
+                    Debug.Log($"Result not found for {sessionNeeded}.");
+                }
+                DateTime currentDate = DateTime.Now;
+
+        // Get the previous date.
+        DateTime previousDate = DateTime.Parse("2024-04-01");
+
+        // Calculate the difference between the two dates.
+        TimeSpan difference = currentDate.Subtract(previousDate);
+
+        // Display the difference.
+            UnityEngine.Debug.Log("The difference between the two dates is days." + difference.Days);
             },
             error =>
             {
