636dd74894634fc8b4d9eea3314903f4b89ea79a
Soham Ovhal <ovhalsoham@gmail.com>
2024-04-10 20:52:46 +0530

    FlappyBirdUI

--- Assets/FlapNFly/Script/GameManager.cs
+++ Assets/FlapNFly/Script/GameManager.cs
@@ -2,33 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;//for storing scores
     public GameObject gameOver;
     public GameObject reload;
     public GameObject menu;
+    public GameObject PausePanel;
     //public GameObject stopScript;
-
 
     int score;
      void Awake()
     {
         gameOver.SetActive(false);
-        reload.SetActive(false);
-        menu.SetActive(false);
-
     }
 
     public void GameOver()
     {
         Debug.Log("HITTTT");
         gameOver.SetActive(true);
-        reload.SetActive(true);
-        menu.SetActive(true);
         Time.timeScale = 0;
 
     }
+    
     public void IncreaseScore()
     {
        Debug.Log("Score got increased");
@@ -36,4 +34,26 @@ public class GameManager : MonoBehaviour
        scoreText.text = score.ToString();//converting our integer to string.
     }
     
+    public void PauseGame()
+    {
+        PausePanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+    //Function to restart a scene
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("FlapNFly");
+        Time.timeScale = 1;
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("GamePanel");
+    }
+    public void ResumeGame()
+    {
+        PausePanel.SetActive(false);
+        Time.timeScale = 1;
+    }
 }
+
